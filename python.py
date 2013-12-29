512d00968969ee3aa51ee642149da0e7b563fe33
Zoltan Lorincz <hawer183@gmail.com>
2013-12-29 18:33:31 +0100

    Very first commit about the python source

---
+++ python.py
@@ -0,0 +1,51 @@
+import time
+import urllib2
+from bs4 import BeautifulSoup
+
+start_time = time.time()
+user_name_array=[]
+def get_fb_username(id):
+    try:        
+        url=urllib2.urlopen('https://graph.facebook.com/'+str(id)).read()
+        soup = BeautifulSoup(url)
+        all_attr=soup.prettify()
+        
+        #print all_attr
+        
+        """Searching for names
+        if all_attr.find('name'):
+            start_quote=all_attr.find('name')+6
+            end_quote=all_attr.find('"',start_quote+1)
+            profile=all_attr[start_quote:end_quote+1].strip('"')
+        End of searching names"""  
+
+        """Searching for location
+        loc=all_attr.find("locale")
+        if (all_attr[loc+12] == 'U'):
+            location='USA'
+        elif (all_attr[loc+12] == 'G'):
+            location = 'UK'
+        else:
+            location="unknow location"
+        End of searching location"""
+                  
+        if all_attr.find('username'):
+            start_quote=all_attr.find('username')+10
+            end_quote=all_attr.find('"',start_quote+1)
+            user_name=all_attr[start_quote:end_quote+1].strip('"')+'@facebook.com'
+            user_name_array.append(user_name)
+            f = open("email2.txt","a")
+            try:
+                f.write (user_name + "\n")
+                #f.write (profile + " - " + user_name + "\n")  
+            finally:
+                f.close()
+    except IOError:
+        pass
+
+for i in range(1,40,1):
+    get_fb_username(i+1)
+print user_name_array
+
+end_time = time.time()
+print("Elapsed time was %g minutes!" % ((end_time - start_time)/60))
